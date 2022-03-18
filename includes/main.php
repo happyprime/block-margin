b5bd780c55b63b9244bcddd4c23989450432af34
Jeremy Felt <jeremy.felt@gmail.com>
2022-03-18 14:03:41 -0700

    Complete reorganization

--- includes/main.php
+++ includes/main.php
@@ -9,11 +9,11 @@ add_action( 'init', __NAMESPACE__ . '\block_supports_init' );
  * Enqueue assets for the block editor.
  */
 function enqueue_block_editor_assets() {
-	$asset_data = require_once __DIR__ . '/build/index.asset.php';
+	$asset_data = require_once dirname( __DIR__ ) . '/build/index.asset.php';
 
 	wp_enqueue_script(
 		'block-margin',
-		plugin_dir_url( __FILE__ ) . 'build/index.js',
+		plugin_dir_url( __DIR__ ) . 'build/index.js',
 		$asset_data['dependencies'],
 		$asset_data['version'],
 		true
