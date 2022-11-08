98853e04f5f90554d83473de6cd2eef293d8a2e9
Jeremy Felt <jeremy.felt@gmail.com>
2022-11-08 14:33:09 -0800

    Apply auto-linting via @wordpress/scripts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'bf55744ae0c7522e3bc9f2b445c8fc25');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'c4153b101c1b2662a31c');
